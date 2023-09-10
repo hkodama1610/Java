c738c6fe8553e0e35f36662c1d82cdd09e1cf5bc
Hiroto Kodama <h.kodama1610@gmail.com>
2023-09-10 17:48:10 +0900

    ログイン機能作成中

--- spring_boot/samuraitravel/src/main/java/com/example/samuraitravel/security/UserDetailsServiceImpl.java
+++ spring_boot/samuraitravel/src/main/java/com/example/samuraitravel/security/UserDetailsServiceImpl.java
@@ -8,27 +8,30 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.example.samuraitravel.entity.User;
 import com.example.samuraitravel.repository.UserRepository;
 
-public class UserDetailsServiceImpl implements UserDetailsService{
-     private final UserRepository userRepository;
-     
-     public UserDetailsServiceImpl(UserRepository userRepository) {
-    	 this.userRepository = userRepository;
-     }
-     
-     @Override
-     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-    	 try {
-    		 User user = userRepository.findByEmail(email);
-    		 String userRoleName = user.getRole().getName();
-    		 Collection<GrantedAuthority> authorities = new ArrayList<>();
-    		 authorities.add(new SimpleGrantedAuthority(userRoleName));
-             return new UserDetailsImpl(user, authorities);
-    	 } catch (Exception e){
-    		 throw new UsernameNotFoundException("ユーザーが見つかりませんでした。");
-    	 }
-     }
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;    
+    
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;        
+    }
+    
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {  
+        try {
+            User user = userRepository.findByEmail(email);
+            String userRoleName = user.getRole().getName();
+            Collection<GrantedAuthority> authorities = new ArrayList<>();         
+            authorities.add(new SimpleGrantedAuthority(userRoleName));
+            return new UserDetailsImpl(user, authorities);
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("ユーザーが見つかりませんでした。");
+        }
+    }   
 }
+
